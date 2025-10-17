dbf2e3f88c12dafd45b3ed7819dadd39054fc675
MdnightStar <axelm200704@gmail.com>
2025-10-16 20:48:55 -0400

    ?
    ?

--- CodigoMobile/IFControl3/app/build.gradle.kts
+++ CodigoMobile/IFControl3/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "ifcontrol.mobile"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
