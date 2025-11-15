141766e0f85625c2ab7db640e83ddfa94172633c
MdnightStar <axelm200704@gmail.com>
2025-11-14 22:29:29 -0400

    Activity ArCond e DS feitas

--- CodigoMobile/IFControl3/app/src/main/java/ifcontrol/mobile/DataShowActivity.java
+++ CodigoMobile/IFControl3/app/src/main/java/ifcontrol/mobile/DataShowActivity.java
@@ -1,4 +1,67 @@
 package ifcontrol.mobile;
 
-public class DataShowActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+public class DataShowActivity extends AppCompatActivity {
+    ImageView imageViewOk;
+    ImageView imageViewCima;
+    ImageView imageViewDireita;
+    ImageView imageViewBaixo;
+    ImageView imageViewEsquerda;
+    ImageView imageViewEsc;
+    ImageView imageViewFreeze;
+    ImageView imageViewMenu;
+    Button buttonVoltar;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datashow);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DataShowActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.config) {
+            Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.sobre) {
+            Toast.makeText(this, "Sobre o app", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
 }
