456a8d96ba0c6f2b7b02de186ae8d66a3be80f91
JeisonAndresP <jeisonpenaclaraterror@gmail.com>
2025-11-18 10:24:19 -0400

    Atualização

--- IF-Control 3.0(Desktop)/src/Aplicacao/FrameSala.java
+++ IF-Control 3.0(Desktop)/src/Aplicacao/FrameSala.java
@@ -283,7 +283,7 @@ public class FrameSala extends javax.swing.JFrame {
         });
 
         comboBoxTemperatura.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 14)); // NOI18N
-        comboBoxTemperatura.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "16", "17", "18", "19", "20", "21", "22", "23", "24", "25" }));
+        comboBoxTemperatura.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "17", "18", "19", "20", "21", "22", "23", "24", "25" }));
         comboBoxTemperatura.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 comboBoxTemperaturaActionPerformed(evt);
